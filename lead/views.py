12cdf562868339fc6f0dd15fd391d4e1fcbfbb4b
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-07 15:37:22 -0400

    Views and forms for leads are generated

---
+++ lead/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.decorators import login_required
+from .forms import AddLeadForm
+from django.shortcuts import render, redirect
+
+
+@login_required
+def add_lead(request):
+    if request.method == 'POST':
+        form = AddLeadForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            return redirect('dashboard')
+
+    else:
+        form = AddLeadForm()
+
+    return render(request, 'lead/add_lead.html', {
+        'form': form
+    })
