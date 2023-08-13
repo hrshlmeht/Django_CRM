be81912e17f624284acfb1bd63324fc8318e6c3c
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-13 00:45:26 -0400

    Converted leads to clients

--- lead/views.py
+++ lead/views.py
@@ -2,13 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import AddLeadForm
-
+from client.models import Client
 from .models import Lead
 
 
 @login_required
 def leads_list(request):
-    leads = Lead.objects.filter(created_by=request.user)
+    leads = Lead.objects.filter(created_by=request.user,converted_to_client=False )
 
     return render(request, 'lead/leads_list.html', {
         'leads': leads
@@ -17,28 +17,28 @@ def leads_list(request):
 
 @login_required
 def leads_detail(request, pk):
-    lead = get_object_or_404(Lead, created_by=request.user,pk=pk)
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
 
     return render(request, 'lead/leads_detail.html', {
         'lead': lead
     })
 
+
 @login_required
-def leads_delete(request ,pk):
+def leads_delete(request, pk):
     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
     lead.delete()
 
-    messages.success(request,'The lead was deleted')
+    messages.success(request, 'The lead was deleted')
 
     return redirect('leads_list')
 
 
-
 @login_required
-def leads_edit(request , pk):
+def leads_edit(request, pk):
     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
     if request.method == 'POST':
-        form = AddLeadForm(request.POST , instance=lead)
+        form = AddLeadForm(request.POST, instance=lead)
         if form.is_valid():
             form.save()
 
@@ -51,8 +51,6 @@ def leads_edit(request , pk):
     return render(request, 'lead/leads_edit.html', {
         'form': form
     })
-
-
 
 
 @login_required
@@ -74,3 +72,22 @@ def add_lead(request):
     return render(request, 'lead/add_lead.html', {
         'form': form
     })
+
+
+@login_required
+def convert_to_client(request, pk):
+    lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
+
+    client = Client.objects.create(
+        name=lead.name,
+        email=lead.email,
+        description=lead.description,
+        created_by = request.user,
+    )
+
+    lead.converted_to_client = True
+    lead.save()
+
+    messages.success(request, 'The lead was converted to client')
+
+    return redirect('leads_list')
