c8749d61e3d8fd71e7ba31c6de92a52e70d3db46
hrshlmeht <harshalmehta1998@gmail.com>
2023-08-25 17:11:12 -0400

    The clients are getting added

--- client/views.py
+++ client/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 , redirect
 from django.contrib.auth.decorators import login_required
 from .models import Client
-
+from .forms import AddClientForm
+from django.contrib import messages
 
 @login_required
 def clients_list(request):
@@ -17,3 +18,25 @@ def clients_detail(request, pk):
     return render(request, 'client/clients_detail.html', {
         'client': client
     })
+
+
+@login_required
+def clients_add(request):
+    if request.method == 'POST':
+        form = AddClientForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            messages.success(request, 'The client is added.')
+            return redirect('clients_list')
+
+    else:
+        form = AddClientForm()
+
+    return render(request, 'client/clients_add.html', {
+        'form': form
+    })
+
